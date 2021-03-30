4b5e65e35f3c5768ae9eb633d7c261a62afc2a21
anup-h <anup.h@berkeley.edu>
2021-03-30 18:39:46 +0000

    got infonce running

--- dataset.py
+++ dataset.py
@@ -5,11 +5,16 @@ import torch
 import os
 import json
 import random
+from torchvision import transforms
+import moviepy.editor as mp
+from PIL import Image
+
 
 class YT8M_RGB(torch.utils.data.Dataset):
-    def __init__(self, root, clip_length):
+    def __init__(self, transform, root, clip_length):
         self.root = root
         self.clip_length = clip_length
+        self.transform = transform
         self.buffer = clip_length
         self.clips = []
         f = open("durations.json","r")
@@ -27,8 +32,26 @@ class YT8M_RGB(torch.utils.data.Dataset):
 
     def __getitem__(self, index):
         vid, l = self.clips[index]
-        v, a, m = torchvision.io.read_video(self.root+vid+'.mp4', l//30, (l+self.clip_length)//30, pts_unit = 'sec')
-        return v[:self.clip_length, :, :, :]
+        
+        v = []
+        cap = cv2.VideoCapture(self.root+vid+'.mp4')
+        cap.set(1, l)
+        for i in range(self.clip_length):
+            _, f = cap.read()
+            v.append(Image.fromarray(cv2.cvtColor(f, cv2.COLOR_BGR2RGB)))
+        v = v*2
+        return torch.stack(self.transform(v), 1)
+        
+#         clip = mp.VideoFileClip(self.root+vid+'.mp4')
+#         v = [Image.fromarray(np.uint8(clip.get_frame(i))).convert('RGB') for i in range(l, l+self.clip_length)]
+#         print(len(v))
+        
+#         v, a, m = torchvision.io.read_video(self.root+vid+'.mp4', l/30, (l+self.clip_length*2)/30, pts_unit='sec')
+#         v = v.permute(0, 3, 1, 2)
+#         assert v.shape[0]>=self.clip_length, str(v.shape)+" "+str(l)+" "+str(self.durations[vid])
+#         v = [transforms.ToPILImage()(v[i]).convert("RGB") for i in range(self.clip_length)]
+#         v = v*2
+#         return torch.stack(self.transform(v), 1)
 
 
 class YT8M_Flow(torch.utils.data.Dataset):
